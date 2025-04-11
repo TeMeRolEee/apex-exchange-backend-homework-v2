7378d241a5d8f9eec26d7b05a7b042b0f5b40c7b
Dávid Réthalmi <david.rethalmi@gmail.com>
2025-04-11 14:14:03 +0200

    base-files: Change maxRetriesPerRequest in redisClient.ts

--- src/server/api/lib/redisClient.ts
+++ src/server/api/lib/redisClient.ts
@@ -1,12 +1,14 @@
 import Redis from "ioredis";
-import { env } from "~/env";
+import { env } from "process";
 
-const redisUrl = env.STATUS_REDIS_URL;
+const redisUrl = env.STATUS_REDIS_URL as string;
 
 let client: Redis;
 
 const initializeRedisClient = () => {
-	client = new Redis(redisUrl);
+	client = new Redis(redisUrl, {
+		maxRetriesPerRequest: null,
+	});
 
 	client.on("error", (err: Error) => {
 		console.error("Redis client error:", err);
